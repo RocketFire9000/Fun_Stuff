4f894a8d859a798eb64820090a8e7210a0b1bcf4
RocketFire9000 <163363346+RocketFire9000@users.noreply.github.com>
2024-03-24 13:52:18 -0500

    1

--- Projectile Motion.py
+++ Projectile Motion.py
@@ -1,25 +1,16 @@
-import pydraw
 import math
 from sys import exit
 print("This program will graph the trajectory of a cannon ball given the variables below. Please enter any positive integer. Do know that higher values may not be visible on the simulation.")
-g = 9.8  # m/s^2 (make this also changeable?)
-vo = int(input("vo: What is the original x velocity? (How fast is it going in meters per second?) "))
-theta_deg = int(input("theta: What angle is it being launched in degrees? Max 90. "))
+g = float(input("What is the gravitational constant in meters per second per second that you want to use? Earth's gravity ia ~9.8 m/s^2. "))
+vo = int(input("What is the original x velocity? (How fast is it going in meters per second?) "))
+theta_deg = int(input("What angle is it being launched in degrees? Max 90. "))
 theta_rad = theta_deg * 0.01745329
 if vo <= 0 or theta_deg <= 0:
     print("Please only use real, positive numbers.")
     exit()
-
-
-def round_up(n, decimals=0):
-    multiplier = 10**decimals
-    return math.ceil(n * multiplier) / multiplier  # Not actually written by me, borrowed from an online tutorial.
-
-
-flight_time = (2 * vo * math.sin(theta_rad)) / g
-final_distance = round_up(vo ** 2 * math.sin(2 * theta_rad) / g, 2)
+flight_time = round((2 * vo * math.sin(theta_rad) / g), 2)
+final_distance = round((vo ** 2 * math.sin(2 * theta_rad) / g), 2)
 final_height = round((vo ** 2) * (math.sin(theta_rad) ** 2) / (2 * g), 2)
 print("The cannon ball will land", final_distance, "meters away")
 print("The cannon ball reached a height of", final_height, "meters above the ground.")
-
-print(round(flight_time, 2))
+print("It was in the air for", flight_time, "seconds.")
