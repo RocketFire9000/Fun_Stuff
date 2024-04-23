61d7c285922d326a2e0f939db13d85c78e827ada
RocketFire9000 <163363346+RocketFire9000@users.noreply.github.com>
2024-04-22 21:32:52 -0500

    1

---
+++ Optics Prob Generator.py
@@ -0,0 +1,13 @@
+from random import randint
+
+di = 0
+do = 0
+m = 0
+f = 0
+hi = 0
+ho = 0
+pr_type = 0
+variables = {"di": di, "do": do, "m": m, "f": f, "hi": hi, "ho": ho}
+print(variables)
+
+# m = -di / do = hi/ ho, 1/f = 1/di + 1/do
